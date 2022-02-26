d5aee3dc1769fe0fa4e16499102a85e908f6c6fc
juniorHotes <juniorhotes53@gmail.com>
2022-02-26 16:54:04 -0300

    Frist commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const {app, autoUpdater, dialog, BrowserWindow} = require('electron');
+
+const url = ``
+autoUpdater.setFeedURL({ url })
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    height: 600,
+    width: 800
+  });
+
+  mainWindow.loadURL('file://' + __dirname + '/index.html', {
+    userAgent: 'foo'
+  });
+});
+
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Restart', 'Later'],
+    title: 'Application Update',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+  }
+dialog.showMessageBox(dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0) autoUpdater.quitAndInstall()
+  })
+})
---
+++ readme.md
@@ -0,0 +1,7 @@
+# service-worker-user-agent-electron-bug
+
+Steps to reproduce:
+
+1. `npm install`
+2. `npm start``
+3. Reload Page to see the service worker `userAgent`
